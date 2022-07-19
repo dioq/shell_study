9e134e0ff0e723617ab2fb212e6ac3d3d7632b9a
Dioq <zhendong2011@live.cn>
2022-07-20 03:20:44 +0800

    读取整个文件并遍历每一行

---
+++ echo_stu.sh
@@ -0,0 +1,7 @@
+
+# 清空原文件再写入自己的数据
+echo "AAAAAAAAA" > ./tmp.txt
+echo "aaa" > ./tmp.txt
+
+# 从原文件尾部追加数据
+echo "BBBBBBBBBB" >> ./tmp.txt
---
+++ loop/while_stu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+i=1
+sum=0
+while ((i <= 100))
+do
+    ((sum += i))
+    ((i++))
+done
+echo "The sum is: $sum"
