c60e6de1f8b45cc98c5656d61b754cfd6c4884b5
dioq <ptttqq@gmail.com>
2024-01-06 14:57:31 +0800

    监控周期改为60s

--- process_stay_active/stay_active.sh
+++ process_stay_active/stay_active.sh
@@ -6,11 +6,10 @@ while true; do
         process=$(ps aux | grep ${exec_path} | grep -v grep)
 
         if [ "$process" == "" ]; then
-                sleep 30
                 echo "process has exited."
                 nohup ${exec_path} &
         else
-                sleep 30
                 echo "process exsits"
         fi
+        sleep 60
 done
