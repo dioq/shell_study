d8069877763f3fba757a2f133b8059493fdc2a7d
dioq <ptttqq@gmail.com>
2024-03-01 15:18:41 +0800

    进程监控

--- process_monitor/watch.sh
+++ process_monitor/watch.sh
@@ -2,14 +2,14 @@
 
 # 检查进程是否存活,如果不存活就重启。可以在定时任务中每分种运行一下这个 shell
 
-work_path=/home/zd/project/main.py
+work_path="$(pwd)"/main.py
 work_log=/var/log/work.log
 watch_log=/var/log/watch.log
 
 process=$(ps aux | grep ${work_path} | grep -v grep)
 if [ "$process" == "" ]; then
         echo "$work_path has exist. It was restarted." >>${watch_log}
-        ${work_path} >>${work_log}
+        nohup ${work_path} >>${work_log} &
 else
         echo "$process" >>${watch_log}
 fi
